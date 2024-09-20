df816b5bb08b087be9f9be6b060142a14bba8517
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-09-20 19:38:39 -0300

    feat: ex1 atualizado

--- vetores_js/ex1/script.js
+++ vetores_js/ex1/script.js
@@ -10,30 +10,61 @@ ser inferiores a R$ 2000,00 e acima de 20000,00.
 Exiba todos os cadastrados na página HTML.*/
 
 function funcionarios() {
-
-   
     let listaFuncionarios = [];
-    let continuar = false;
+    let continuar = true;
+    let contador = 0;
     do { 
-        let info = [];
+        contador ++;
+
+        let info = {};
         let nome = prompt("Informe o nome e sobrenome do funcionário:");
         nome = nome.trim();
         while (isNaN(nome) === false || nome.indexOf(" ") < 3) {
             alert("Nome inválido, tente novamente.");
-            nome = prompt("Infome mais um funcionário:");
+            nome = prompt("Informe o nome e sobrenome do funcionário novamente:");
         }
         info["nome"] = nome;
 
         let idade = prompt("Informe a idade do funcionário:");
-        while (isNaN(idade) || idade <= 14 || idade >=120) {
+        while (isNaN(idade) || idade <= 14 || idade > 120) {
             alert("Idade inválida, tente novamente.");
-            idade = prompt("Informe a idade do funcionário:");
+            idade = prompt("Informe a idade do funcionário novamente:");
         }
+        info["idade"] = idade;
 
-        listaFuncionarios.push(info)
+        let salario = prompt("Informe o salário do funcionário:");
+        while (isNaN(salario) || salario < 2000 || salario >= 20000) {
+            alert("Salário inválido! Deve ser entre R$2.000 e R$20.000");
+            salario = prompt("Informe o salário do funcionário novamente:");
+        }
+        info["salario"] = salario;
 
+        listaFuncionarios.push(info);
+        if (contador < 5) {
+            continuar = true;
+        } else if (contador >=5 && contador < 100) {
+        continuar = confirm("Deseja adicionar mais um funcionário?");
+        } else {
+            continuar = false;
+        }
     }
     while (continuar);
 
 }
+function exibir(listaempregados) {
+    let mensagem ="";
+    for (let i = 0; i < listaempregados.length; i++) {
+        let emp = listaempregados[i];
+        mensagem += `
+        Nome: ${emp["nome"]} <br/>
+        Idade: ${emp["idade"]} anos <br/>
+        Salário: R$ ${parseFloat(emp["salário"]).toFixed(2)} <br/>
+        <br/><br/>
+        `;
+        
+    }
+    document.write(mensagem);
+}
+console.log(listaFuncionarios);
+
 funcionarios();
--- vetores_js/ex2/script.js
+++ vetores_js/ex2/script.js
@@ -12,38 +12,3 @@ podem ser inferiores a 0 e acima de 10.
 • “N1: 0,00 / N2: 0,00 / N3: 0,00 / Media: 0,00”. 
 Exiba todos os cadastrados na página HTML*/
 
-/* 
-Ex4: Crie um algoritmo usando funções que tenha um menu de cadastro de nomes 
-e salários bruto de até 50 pessoas, e ao final, mostre os nomes, 
-salários brutos e os salários líquidos de todos que foram lidos, 
-considerando 10% de INSS a descontar.
-*/
-
-function funcionarios() {
-    let listaFuncionarios = [];
-    let continuar = true;
-    do { 
-        let info = {};
-        let nome = prompt("Informe o nome e sobrenome do funcionário:");
-        nome = nome.trim();
-        while (isNaN(nome) === false || nome.indexOf(" ") < 3) {
-            alert("Nome inválido, tente novamente.");
-            nome = prompt("Informe o nome e sobrenome do funcionário:");
-        }
-        info["nome"] = nome;
-
-        let idade = prompt("Informe a idade do funcionário:");
-        while (isNaN(idade) || idade <= 0) {
-            alert("Idade inválida, tente novamente.");
-            idade = prompt("Informe a idade do funcionário:");
-        }
-        info["idade"] = idade;
-
-        listaFuncionarios.push(info);
-        continuar = confirm("Deseja adicionar mais um funcionário?");
-    }
-    while (continuar);
-
-    console.log(listaFuncionarios);
-}
-funcionarios();
